286da451e9439918fc5c52a1e5315a566f1cc7fa
SmitUdani <smitudani04@gmail.com>
2025-01-09 00:04:57 +0530

    [Respond with 200]

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,12 +5,18 @@ fun main() {
     println("Logs from your program will appear here!")
 
     // Uncomment this block to pass the first stage
-     val serverSocket = ServerSocket(4221)
+    val serverSocket = ServerSocket(4221)
 
-     // Since the tester restarts your program quite often, setting SO_REUSEADDR
-     // ensures that we don't run into 'Address already in use' errors
-     serverSocket.reuseAddress = true
+    // Since the tester restarts your program quite often, setting SO_REUSEADDR
+    // ensures that we don't run into 'Address already in use' errors
+    serverSocket.reuseAddress = true
 
-     serverSocket.accept() // Wait for connection from client.
-     println("accepted new connection")
+    val client = serverSocket.accept() // Wait for connection from client
+    val outputStream = client.getOutputStream()
+
+    outputStream.write("HTTP/1.1 200 OK\\r\\n\\r\\n".toByteArray())
+    outputStream.flush()
+    outputStream.close()
+
+    println("accepted new connection")
 }
